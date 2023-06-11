54066c7966eccec53906f731e11eac142656f054
mitsudaman <k.m.masato-uver-aquarius@hotmail.co.jp>
2023-06-12 08:56:33 +0900

    all commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Folder;
+use App\Models\Task;
+use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class TaskController extends Controller
+{
+    public function index(int $id,Request $request)
+    {
+        $folders = Folder::all();
+
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐づくタスクを取得する
+        // $tasks = $current_folder->tasks()->get(); 
+        $sort = $request->sort;
+        var_dump($request->query());
+        $tasks = Task::paginate(5);
+
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
+            'sort' => $sort
+        ]);
+    }
+    /**
+     * GET /folders/{id}/tasks/create
+     */
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
+        ]);
+    }
+
+    /**
+     * GET /folders/{id}/tasks/{task_id}/edit
+     */
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
+
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+        // 1
+        $task = Task::find($task_id);
+
+        // 2
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        // 3
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
+        ]);
+    }
+
+    public function delete(int $id, int $task_id)
+    {
+        // 1
+        $task = Task::find($task_id);
+
+        // 2
+        $task->delete();
+
+        // 3
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
+        ]);
+    }
+
+    public function upload(Request $request)
+    {
+        // ディレクトリ名
+        $dir = 'sample';
+
+        // アップロードされたファイル名を取得
+        $file_name = $request->file('image')->getClientOriginalName();
+
+        // 取得したファイル名で保存
+        $request->file('image')->storeAs('public' , $file_name);
+
+        var_dump($file_name);
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+ 
+Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@create');
+Route::get('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@edit');
+Route::get('/folders/{id}/tasks/{task_id}/delete', 'App\Http\Controllers\TaskController@delete')->name('tasks.delete');
