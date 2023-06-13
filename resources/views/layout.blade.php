6abd1d759e97bc6f027f7a1d34417c6a56e06656
mitsudaman <k.m.masato-uver-aquarius@hotmail.co.jp>
2023-06-13 09:10:11 +0900

    design update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,14 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>ToDo App</title>
+  <title>LineADay</title>
   @yield('styles')
   <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
 <header>
   <nav class="my-navbar">
-    <a class="my-navbar-brand" href="/">ToDo App</a>
+    <a class="my-navbar-brand" href="/">LineADay</a>
   </nav>
 </header>
 <main>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,28 +3,15 @@
 @section('content')
   <div class="container">
     <div class="row">
-      <div class="col col-md-4">
-        <nav class="panel panel-default">
-          <div class="panel-heading">フォルダ</div>
-          <div class="list-group">
-            @foreach($folders as $folder)
-              <a
-                  href="{{ route('tasks.index', ['id' => $folder->id]) }}"
-                  class="list-group-item {{ $current_folder_id === $folder->id ? 'active' : '' }}"
-              >
-                {{ $folder->title }}
-              </a>
-            @endforeach
-          </div>
-        </nav>
+      <div class="col col-md-3">
       </div>
-      <div class="column col-md-8">
+      <div class="column col-md-6">
         <div class="panel panel-default">
-          <div class="panel-heading">タスク</div>
+          <div class="panel-heading">Diary</div>
           <div class="panel-body">
             <div class="text-right">
               <a href="{{ route('tasks.create', ['id' => $current_folder_id]) }}" class="btn btn-default btn-block">
-                タスクを追加する
+                日記を追加する
               </a>
             </div>
           </div>
@@ -32,8 +19,7 @@
             <thead>
             <tr>
               <th>画像</th>
-              <th>タイトル</th>
-              <th>期限</th>
+              <th>日記</th>
               <th></th>
               <th></th>
             </tr>
@@ -41,17 +27,16 @@
             <tbody>
             @foreach($tasks as $task)
               <tr>
-                <td>
-                  <img src="{{ Storage::url($task->img_path) }}" width="25%">
+                <td class="col-md-2">
+                  <img src="{{ Storage::url($task->img_path) }}" width="100%">
                 </td>
-                <td>{{ $task->title }}</td>
-                <td>{{ $task->formatted_due_date }}</td>
-                <td>
+                <td class="col-md-8">{{ $task->title }}</td>
+                <td class="col-md-1">
                 <a href="{{ route('tasks.edit', ['id' => $task->folder_id, 'task_id' => $task->id]) }}">
                   編集
                 </a>
                 </td>
-                <td>
+                <td class="col-md-1">
                 <a href="{{ route('tasks.delete', ['id' => $task->folder_id, 'task_id' => $task->id]) }}">
                   削除
                 </a>
@@ -65,6 +50,7 @@
             {{ $tasks->links() }}
         </div>
       </div>
+      <div class="column col-md-3"></div>
     </div>
   </div>
 @endsection
